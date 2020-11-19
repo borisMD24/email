2a5ced2f9799a8db383b165fa85bf95eca6231a5
rtdeedqgryet <borismariondorier@gmail.com>
2020-11-19 19:38:11 +0100

    first commit

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,39 @@
+class EmailsController < ApplicationController
+    def show
+      @email = Email.all.find(params[:id])
+    end
+    def new
+      @emails = Email.all
+    end
+  
+    def create
+      @email = Email.new()
+      @email.object = Faker::Color.color_name
+      @email.body = Faker::GreekPhilosophers.quote
+      
+      if @email.save
+        flash[:notice] = "Email added"
+        respond_to do |format|
+            format.html { redirect_to emails_path}
+            format.js { }
+        end
+        
+      else
+          flash[:notice] = "Please try again"
+        respond_to do |format|
+            format.html { redirect_to emails_path}
+            format.js { }
+        end
+      end
+      
+    end
+    def destroy
+      @email.destroy
+      respond_to do |format|
+        format.html { redirect_to emails_path }
+        format.js {}
+      end
+  end
+  
+  
+  end
